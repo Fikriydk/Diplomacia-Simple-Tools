3bd6cb08bb1cc4ba4bbc30b335d833727ec53c8d
Fikriydk <fikriydwik@gmail.com>
2026-09-16 02:06:08 +0700

    Fix count profitable premium six month feature

--- convertd.py
+++ convertd.py
@@ -39,22 +39,24 @@ class infoData():
 			return (viewM)
 		else:
 			print("You must input all require data")
+			
 class premData():
 	durMin = 11
+	pOneM = None
+	workTime = None
 
-	def __init__ (self, priceAmonth, price6month, balAmonth, bal6month, perHit, price1m):
+	def __init__ (self, priceAmonth, price6month, balAmonth, bal6month, perHit):
 		self.pAmonth = priceAmonth
 		self.p6month = price6month
 		self.bAmonth = balAmonth
 		self.b6month = bal6month
 		self.Ahit = perHit
-		self.p1m = price1m
 
 	def monthPrem (self):
 		if self.p6month is None and self.b6month is None:
-			dayGross = (1440/premData.durMin) * self.Ahit
-			dcBalance = dayGross - (self.bAmonth/30)
-			dcPrice = (dcBalance / 1000000) * self.p1m #buat variabel untuk harga mata uang-nya
+			dayGross = ((premData.workTime * 60) / premData.durMin) * self.Ahit
+			dcBalance = dayGross - (self.bAmonth / 30)
+			dcPrice = (dcBalance / 1000000) * premData.pOneM
 			dcpMonthly = dcPrice * 30
 			dcbMonthly = (dayGross * 30) - self.bAmonth
 			viewProfit = f"""Daily price profit : Rp {dcPrice:,.0f}\nbalance profit : $ {dcBalance:,.0f}\nMonthly price profit : Rp {dcpMonthly:,.0f}\nMonthly balance profit : $ {dcbMonthly:,.0f}""".replace(',','.')
@@ -62,59 +64,59 @@ class premData():
 			
 	def sixMonthPrem (self):
 		if self.pAmonth is None and self.bAmonth is None:
-			dayGross = (1440/premData.durMin) * self.Ahit
+			dayGross = (( premData.workTime*60 )/premData.durMin) * self.Ahit
 			dcBalance = dayGross - (self.b6month/(30*6))
-			dcPrice = (dcBalance / 1000000) * self.p1m #buat variabel untuk harga mata uang-nya
+			dcPrice = (dcBalance / 1000000) * premData.pOneM
 			dcpSixMonthly = dcPrice * (30*6)
 			dcbSixMonthly = (dayGross * (30*6)) - self.b6month
-			viewProfit = f"""Daily price profit : Rp {dcPrice:,.0f}\nbalance profit : $ {dcBalance:,.0f}\nSix monthly price profit : Rp {dcpMonthly:,.0f}\nSix monthly balance profit : $ {dcbMonthly:,.0f}""".replace(',','.')
+			viewProfit = f"""Daily price profit : Rp {dcPrice:,.0f}\nbalance profit : $ {dcBalance:,.0f}\nSix monthly price profit : Rp {dcpSixMonthly:,.0f}\nSix monthly balance profit : $ {dcbSixMonthly:,.0f}""".replace(',','.')
 			return (viewProfit)
 
 print(intro)
-priceAdm = float(input("Input number of balance avg for 1dm in markets: "))
+priceAdm = int(input("Input number of balance avg for 1dm in markets: "))
 
 print(mainMenu)
 inputOption = int(input("Type your selection: "))
 
 if inputOption == 1:
-	DM = float(input("Input your number of diamonds: "))
+	DM = int(input("Input your number of diamonds: "))
 	M = None
 	covData = infoData(DM, M)
 	resultData = covData.convertData()
 	print("$", resultData)
 
 elif inputOption == 2:
-	M = float(input("Input you number of balance: "))
+	M = int(input("Input you number of balance: "))
 	DM = None
 	covData = infoData(DM, M)
 	resultData = covData.convertData()
 	print(resultData, "DM")
 
 elif inputOption == 3:
+	premData.pOneM = int(input("how much does one M cost in the market today: "))
+	premData.workTime = int(input("How long do you work until the resources run out [ hours ]: "))
 	print(countIntro)
 	inputOptionCount = int(input("Type your selection: "))
 
 	if inputOptionCount == 1:
-		poneMonth = float(input("Input market price for premium: "))
-		boneMonth = float(input("Input market balance for premium: "))
-		oneHit = float(input("Input balance you get for one hit: "))
-		PriceM = float(input("Input market price (real money) for 1M: "))
+		poneMonth = int(input("Input market price for premium: "))
+		boneMonth = int(input("Input market balance for premium: "))
+		oneHit = int(input("Input balance you get for one hit: "))
 		psixMonth = None
 		bsixMonth = None
 
-		monthlyPrem = premData(poneMonth, psixMonth, boneMonth, bsixMonth, oneHit, PriceM)
+		monthlyPrem = premData(poneMonth, psixMonth, boneMonth, bsixMonth, oneHit)
 		profmPrem = monthlyPrem.monthPrem()
 		print(f"""\n[ RESULT ]\n{profmPrem}""")
 
 	elif inputOptionCount == 2:
-		psixMonth = float(input("Input market price for premium: "))
-		bsixMonth = float(input("Input market balance for premium: "))
-		oneHit = float(input("Input balance you get for one hit: "))
-		PriceM = float(input("Input market price (real money) for 1M: "))
+		psixMonth = int(input("Input market price for premium: "))
+		bsixMonth = int(input("Input market balance for premium: "))
+		oneHit = int(input("Input balance you get for one hit: "))
 		poneMonth = None
 		boneMonth = None
 
-		sixMonthlyPrem = premData(poneMonth, psixMonth, boneMonth, bsixMonth, oneHit, PriceM)
+		sixMonthlyPrem = premData(poneMonth, psixMonth, boneMonth, bsixMonth, oneHit)
 		profmPrem = sixMonthlyPrem.sixMonthPrem()
 		print(f"""\n[ RESULT ]\n{profmPrem}""")
 	else:
